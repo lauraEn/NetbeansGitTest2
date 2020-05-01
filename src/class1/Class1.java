d9129c3cadf502083af74842db86a1ce8a339a08
LauraPC <LauraPC@10.0.0.74>
2020-05-01 17:23:36 +0200

    Great commit

--- src/class1/Class1.java
+++ src/class1/Class1.java
@@ -4,8 +4,7 @@ package class1;
 public class Class1 {
 
     public static void main(String[] args) {
-        Class2 c2 = new Class2();
-        c2.m();
+        System.out.println("main");
     }
 }
 
